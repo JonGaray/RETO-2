60d4a3a6bbaf4f681d1b96265a36a576617a0f49
GARAYYY <jongaray7@gmail.com>
2025-01-14 15:43:30 +0100

    edit campuses

--- proyecto-backend/app/Http/Controllers/CampusController.php
+++ proyecto-backend/app/Http/Controllers/CampusController.php
@@ -30,4 +30,25 @@ class CampusController extends Controller
             'maquina' => $machine,
         ]);
     }
+    public function edit(Request $request, $id): \Illuminate\Http\JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'status' => 'required|in:habilitado,deshabilitado',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 400);
+        }
+        $campus = Campus::find($id);
+        if (!$campus) {
+            return response()->json(['message' => 'Campus no encontrado'], 404);
+        }
+        $campus->name = $request->name;
+        $campus->status = $request->status;
+        $campus->save();
+        return response()->json([
+            'message' => 'Campus actualizado correctamente',
+            'maquina' => $campus,
+        ]);
+    }
 }
