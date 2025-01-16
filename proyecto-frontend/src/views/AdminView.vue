98d300739904fc1317a7dc3b48ecbd7a1ce3efab
JonGaray <jon.garay@ikasle.egibide.org>
2025-01-16 12:47:12 +0100

    seleccion de seccion en campus

--- proyecto-frontend/src/views/AdminView.vue
+++ proyecto-frontend/src/views/AdminView.vue
@@ -4,6 +4,7 @@ import AdminPagePanel from '../components/AdminPagePanel.vue';
 import AdminUserPanel from '../components/AdminUserPanel.vue';
 import AdminMachinePanel from '../components/AdminMachinePanel.vue'
 import AdminCampusPanel from '@/components/AdminCampusPanel.vue';
+import AdminSectionPanel from '@/components/AdminSectionPanel.vue';
 </script>
 
 <template>
@@ -17,7 +18,7 @@ import AdminCampusPanel from '@/components/AdminCampusPanel.vue';
             <AdminPagePanel />
         </div>
         <div class="col-8">
-          <AdminCampusPanel />
+          <AdminSectionPanel />
         </div>
       </div>
     </div>
