e997221c73331bad68b7e5e19e02b3d9a66212e6
JonGaray <jon.garay@ikasle.egibide.org>
2025-01-16 10:56:19 +0100

    editar perfil eliminado

--- proyecto-frontend/src/components/UserPanel.vue
+++ proyecto-frontend/src/components/UserPanel.vue
@@ -6,12 +6,9 @@
           <img src="../img/foto-default.png" alt="Foto de perfil" class="rounded-circle img-fluid foto-perfil-usu">
           <h5 class="fw-bold mb-0 text-nowrap">{{ userName }}</h5>
         </div>
-        <div class="d-flex justify-content-between">
+        <div class="d-flex justify-content-between mt-5">
           <span class="fw-bold">Incidencias realizadas:</span>
           <span>{{ incidentsCount }}</span>
-        </div>
-        <div class="d-flex justify-content-center">
-          <button class="btn btn-egibide mt-3 boton-perfil-usu">Ver perfil</button>
         </div>
       </div>
     </div>
